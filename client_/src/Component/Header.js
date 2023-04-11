0da9deba9363bf969af59fe2a06725239c8e0b4d
Ahmad <Ahmad>
2023-04-11 12:17:32 +0530

    commit_browse

--- client_/src/Component/Header.js
+++ client_/src/Component/Header.js
@@ -1,9 +1,15 @@
 import React from 'react'
-
+import Menu from './Menu_icon.png'
+import Compicon from './company.png';
 export default function Header() {
   return (
-    <div className='Header'>
-
+    <div className="Header">
+      <div className="firsthalf">
+        <img src={Menu} id="menu" />
+        <img src={Compicon} id="comp" />
+        <h3 id='hedh_3'>CipherSchools</h3>
+      </div>
+      <div className="secondhalf"></div>
     </div>
-  )
+  );
 }
