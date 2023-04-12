9c44a5757e17002784000310e606a3222c5d15b2
Ahmad <Ahmad>
2023-04-12 15:59:20 +0530

    commit_for_singup_design

---
+++ client_/src/Component/Pages/Seconddiv.js
@@ -0,0 +1,59 @@
+import React from 'react'
+import './second_div.css';
+import CloseIcon from "@mui/icons-material/Close";
+import Compicon from "../company.png";
+export default function Seconddiv({props}) {
+  var list_=["First Name","Last Name","Email Address","Phone(Optional)","Password"];
+  return (
+    <div id="ssame" onClick={props}>
+      <div id="crosshead">
+        Sign In <CloseIcon style={{ fontSize: "42px", fontWeight: "800" }} />{" "}
+      </div>
+      <div id="Ssignheading">
+        <h3 id="sssh1">
+          {" "}
+          <img src={Compicon} id="sshimg" /> CipherSchools{" "}
+        </h3>
+        <h4 id="ssh2"> Create New Account</h4>
+        <p id="ssp">Please provide your valid informations to signup</p>
+      </div>
+
+      <div id="buttinp">
+        {list_.map((e, i) => {
+          if (i <4 ) {
+            return <input id="inpsame" placeholder={e} key={i} />;
+          } else  if(i===4) {
+            return (
+              <div id="inpsame" key={i}>
+                <input
+                  style={{
+                    width: "80%",
+                    height: "100%",
+                    fontSize: "20px",
+                    background: "transparent",
+                    border: "none",
+                    outline: "none",
+                  }}
+                  placeholder={e}
+                />
+              </div>
+            );
+          }
+        })}
+        {/* <h5 id="ssih5">Forget Password ?</h5> */}
+        <button id="sssbtn">Create Account</button>
+        {/* <p id="ssip">
+          {" "}
+          Don't have an account ?{" "}
+          <span
+            className="span_"
+            onClick={props}
+            style={{ color: "orange", cursor: "pointer" }}
+          >
+            Get Started
+          </span>
+        </p> */}
+      </div>
+    </div>
+  );
+}
--- client_/src/Component/Pages/SignDiv.js
+++ client_/src/Component/Pages/SignDiv.js
@@ -1,80 +1,43 @@
 import React, { useState } from "react";
-import Compicon from "../company.png";
+// import Compicon from "../company.png";
 
-import CloseIcon from "@mui/icons-material/Close";
+// import CloseIcon from "@mui/icons-material/Close";
+import Firstdivsign from "./Firstdivsign";
+import Seconddiv from "./Seconddiv";
 export default function SignDiv() {
   var [seton, setoff_] = useState("on_1");
-  var list_=["Email ID","Password"];
+  // var list_=["Email ID","Password"];
     function clickHandler(e)
     {
-        var a = e.target.parentElement;
-        // console.log(e.target.innerText);
-        if (e.target.innerText === "Sign in"){
-            a.style.border = "4px solid red";
+        
+        var a;
+        console.log(e.target.className);
+        if (e.target.className==="span_"){
+          a = e.target.parentElement.parentElement.parentElement.parentElement;
+        }
+        else{
+          a=e.target.parentElement;
+        }
+          if (e.target.className === "span_") {
+            a.style.top = "12%";
+            a.style.height = "80%";
+            setoff_("");
+          } else {
             a.style.height = "70%";
             a.style.top = "15%";
             console.log(a);
-            setoff_("");
-        } 
-        else{
-            // var parent = e.target.parentElement;
-            // parent.style.height="70%";
-            console.log(a)
-            a.style.border = "4px solid green";
-            a.style.top="12%";
-            a.style.height="80%";
             setoff_("on");
-            
-        }
+          }
     }
   return (
     <div className="signin_div">
       {seton ? (
-        <div id="ssame" onClick={clickHandler}>
-          <div id="crosshead">
-            Sign In{" "}
-            <CloseIcon style={{ fontSize: "42px", fontWeight: "800" }} />{" "}
-          </div>
-          <div id="signheading">
-            <h3 id="ssh1">
-              {" "}
-              <img src={Compicon} id="sshimg" />{" "}
-              CipherSchools{" "}
-            </h3>
-            <h4 id="ssh2"> Hey, Welcome!</h4>
-            <p id="ssp">Please provide your email and password to signin</p>
-          </div>
-          <div id="buttinp">
-            {list_.map((e, i) => {
-              if (i === 0) {
-                return <input id="inpsame" placeholder={e} key={i} />;
-              } else {
-                return (
-                  <div id="inpsame" key={i}>
-                    <input
-                      style={{
-                        width: "80%",
-                        height: "100%",
-                        fontSize: "20px",
-                        background: "transparent",
-                        border: "none",
-                        outline: "none",
-                      }}
-                      placeholder={e}
-                    />
-                  </div>
-                );
-              }
-            })}
-            <h5 id="ssih5">Forget Password ?</h5>
-            <button id="ssbtn">Sign in</button>
-            <p id="ssip"> Don't have an account ? <span style={{color:"orange",cursor:"pointer"}}>Get Started</span></p>
-          </div>
-        </div>
+        <Firstdivsign props={clickHandler} />
       ) : (
-        <div id="ssame" onClick={clickHandler}>
-          sign up
-        </div>
+        // <div id="ssame" onClick={clickHandler}>
+        //   sign up
+        // </div>
+        <Seconddiv props={clickHandler} />
       )}
     </div>
   );
